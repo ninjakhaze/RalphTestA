620b63749ad345269c47b16b296acb908bff9d8d
Ralph <tongolralph@yahoo.ca>
2020-10-31 08:57:57 -0400

    Initial commit

---
+++ ralphTestA/src/main/java/ralph/tongol/s300893239/RalphActivity.java
@@ -0,0 +1,21 @@
+package ralph.tongol.s300893239;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class RalphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ralph);
+    }
+
+}
